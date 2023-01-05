2fc7398c628741e5a6483d15eeb96d69cbd544ad
Kimhannah <skqkdldhf98@naver.com>
2023-01-05 15:25:59 +0900

    Assignment 9 : Add background using linear-gradient()

---
+++ assignmets/randomness/src/index.js
@@ -0,0 +1,35 @@
+const colors = [
+  "#ef5777",
+  "#575fcf",
+  "#4bcffa",
+  "#34e7e4",
+  "#0be881",
+  "#f53b57",
+  "#3c40c6",
+  "#0fbcf9",
+  "#00d8d6",
+  "#05c46b",
+  "#ffc048",
+  "#ffdd59",
+  "#ff5e57",
+  "#d2dae2",
+  "#485460",
+  "#ffa801",
+  "#ffd32a",
+  "#ff3f34"
+];
+
+const button = document.querySelector("button");
+
+function selectColors() {
+	let selected = [
+		colors[Math.floor(Math.random() * colors.length)],
+		colors[Math.floor(Math.random() * colors.length)],
+	];
+	while (selected[0] === selected[1])
+		selected[1] = colors[Math.floor(Math.random() * colors.length)];
+
+	document.body.style.background = `linear-gradient(${selected[0]}, ${selected[1]})`;
+}
+
+button.addEventListener("click", selectColors);
